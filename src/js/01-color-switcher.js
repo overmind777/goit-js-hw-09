b3a94a1d7df6f33cc4fa3ba8c9fa6d6c1f0767ec
overmind777 <63595243+overmind777@users.noreply.github.com>
2023-09-24 22:20:58 +0300

    create 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const body = document.querySelector('body');
+const btnStart = body.querySelector('button[data-start]');
+const btnStop = body.querySelector('button[data-stop]');
+let timerId = null;
 
+btnStart.addEventListener('click', e => {
+  btnStart.disabled = true;
+  btnStop.disabled = false;
+  timerId = setInterval(() => {
+    body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+});
+
+btnStop.addEventListener('click', () => {
+  btnStop.disabled = true;
+  btnStart.disabled = false;
+  clearInterval(timerId);
+});
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
